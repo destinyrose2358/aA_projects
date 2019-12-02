c49155de0a86d832f3b4b047e25fea2519132f61
Destiny Rose <destinyrose2358@gmail.com>
2019-12-02 03:19:42 -0600

    Adds quick sort porject

--- W22D5/quick_sort_project/lib/quick_sort.js
+++ W22D5/quick_sort_project/lib/quick_sort.js
@@ -1,5 +1,16 @@
 function quickSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
 
+    let pivot = array.shift();
+    let left = array.filter(el => el < pivot);
+    let right = array.filter(el => el >= pivot);
+
+    let leftSort = quickSort(left);
+    let rightSort = quickSort(right);
+
+    return [...leftSort, pivot, ...rightSort];
 }
 
 
