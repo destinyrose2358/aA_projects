b9d6a265caadf36f166227d28bb4480c056905c3
Destiny Rose <destinyrose2358@gmail.com>
2019-12-02 02:33:29 -0600

    Finishes counting sort project

--- W22D5/counting_sort_project/lib/counting_sort.js
+++ W22D5/counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,18 @@
 function countingSort(arr, max) {
+    const results = [];
+    const counts = new Array(max + 1).fill(0);
 
+    for (let idx = 0; idx < arr.length; idx++) {
+        counts[arr[idx]]++;
+    }
+
+    for (let idx = 0; idx < counts.length; idx++) {
+        while (counts[idx] > 0) {
+            results.push(idx);
+            counts[idx]--;
+        }
+    }
+    return results;
 }
 
 
