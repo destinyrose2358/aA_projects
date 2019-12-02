db32493f61bf41e3d4af939d3574ffe2546e5f16
Destiny Rose <destinyrose2358@gmail.com>
2019-12-02 02:25:58 -0600

    Adds selection sort project

--- W22D4/selection_sort_project/lib/selection_sort.js
+++ W22D4/selection_sort_project/lib/selection_sort.js
@@ -1,9 +1,19 @@
 function swap(arr, index1, index2) {
-
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]]
+    return arr;
 }
 
 function selectionSort(arr) {
+    for (let idx = 0; idx < arr.length; idx++) {
+        let minimumIdx = idx;
 
+        for (let unsortedIdx = idx + 1; unsortedIdx < arr.length; unsortedIdx++) {
+            if (arr[minimumIdx] > arr[unsortedIdx]) {
+                minimumIdx = unsortedIdx;
+            }
+        }
+        swap(arr, idx, minimumIdx);
+    }
 }
 
 module.exports = {
