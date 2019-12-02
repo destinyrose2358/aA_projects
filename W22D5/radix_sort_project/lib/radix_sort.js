cde48b345650d4527eabb8044ac1b769061d14af
Destiny Rose <destinyrose2358@gmail.com>
2019-12-02 03:31:38 -0600

    Adds radix sort project

--- W22D5/radix_sort_project/lib/radix_sort.js
+++ W22D5/radix_sort_project/lib/radix_sort.js
@@ -1,5 +1,36 @@
 function radixSort(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
 
+    let max = getMax(arr);
+    for (let idx = 0; idx < max; idx++) {
+        let buckets = Array.from({ length: 10 }, () => []); // Array of empty arrays
+
+        for (let i = 0; i < arr.length; i++) {
+            let digit = getDigit(arr[i], idx);
+            buckets[digit].push(arr[i]);
+        }
+
+        arr = [].concat(...buckets);
+    }
+    return arr;
+}
+
+function getMax(arr) {
+    let max = 0;
+    for (let idx = 0; idx < arr.length; idx++) {
+        max = Math.max(max, getIntLength(arr[idx]));
+    }
+    return max;
+}
+
+function getIntLength(num) {
+    return (num === 0) ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function getDigit(num, place) {
+    return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
 }
 
 module.exports = {
