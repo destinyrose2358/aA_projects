cc3647e400cdb9ceacbb2e42f5e1b58d01f36f98
Destiny Rose <destinyrose2358@gmail.com>
2019-12-02 03:17:22 -0600

    Adds merge sort project

--- W22D5/merge_sort_project/lib/merge_sort.js
+++ W22D5/merge_sort_project/lib/merge_sort.js
@@ -1,9 +1,32 @@
 function merge(array1, array2) {
+    const merged = [];
 
+    while (array1.length || array2.length) {
+        let element1 = array1.length ? array1[0] : Infinity;
+        let element2 = array2.length ? array2[0] : Infinity;
+
+        let next;
+        if (element1 > element2) {
+            next = array2.shift();
+        } else {
+            next = array1.shift();
+        }
+        merged.push(next);
+    }
+    return merged;
 }
 
 function mergeSort(array) {
+    if (array.length <= 1) return array;
 
+    let middle = Math.floor(array.length / 2);
+    let left = array.slice(0, middle);
+    let right = array.slice(middle);
+
+    let leftSort = mergeSort(left);
+    let rightSort = mergeSort(right);
+
+    return merge(leftSort, rightSort);
 }
 
 module.exports = {
