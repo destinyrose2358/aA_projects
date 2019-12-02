bb5b769d7c99e15f588ed65bb8ace0b9706ca0f6
Destiny Rose <destinyrose2358@gmail.com>
2019-12-02 02:20:16 -0600

    Adds insertion sort project

--- W22D4/insertion_sort_project/lib/insertion_sort.js
+++ W22D4/insertion_sort_project/lib/insertion_sort.js
@@ -1,5 +1,13 @@
 function insertionSort(arr) {
-
+    for (let idx = 1; idx < arr.length; idx++) {
+        let currentEl = arr[idx];
+        let testIdx;
+        for (testIdx = idx - 1; testIdx >= 0 && currentEl < arr[testIdx]; testIdx--) {
+            arr[testIdx + 1] = arr[testIdx];
+        }
+        arr[testIdx + 1] = currentEl;
+    }
+    return arr;
 }
 
 module.exports = {
