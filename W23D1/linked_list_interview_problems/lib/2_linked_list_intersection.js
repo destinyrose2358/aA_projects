1e6752070b12a2eec45ea256d5b00f6257236f7c
Destiny Rose <destinyrose2358@gmail.com>
2019-12-02 21:36:05 -0600

    Adds linked list interview problems

---
+++ W23D1/linked_list_interview_problems/lib/2_linked_list_intersection.js
@@ -0,0 +1,122 @@
+// ============================================================================
+// Interview Problem: Linked List Intersection
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Write a function linkedListIntersection that returns the node at which the 
+// intersection of two linked lists begins, or null if there is no such 
+// intersection.
+//
+// ---------- 
+// Example 1:
+// ----------
+// 
+// Given the following two linked lists, list1 and list2, 
+// linkedListIntersection(list1,list2) should return D 
+// as the node of intersection.
+// 
+//    A → B → C
+//             ↘
+//               D → E → F
+//             ↗
+//        X → Y
+//
+// ---------- 
+// Example 2:
+// ----------
+//
+// Given the following two linked lists, list1 and list2, 
+// linkedListIntersection(list1, list2) should return null 
+// as there is no point of intersection.
+// 
+//    A → B → C → D
+//
+//    X → Y → Z
+// 
+// -----------
+// Let's code!
+// -----------
+function linkedListIntersection(list1, list2) {
+  // TODO: Implement the hasCycle function!
+
+  //exploitable test
+  if (list1.tail.next) return list1.tail.next;
+  if (list2.tail.next) return list2.tail.next;
+  return null;
+
+  //other solution not possible but psuedocode provided
+  //find the offset for each list
+  // ex list1: A -> B -> C -> D
+  // length = 4
+  // list2: X -> Y -> Z -> C -> D
+  // length = 5
+  // list1offset = 0
+  // list2offset = 1
+  // get node 0 of list1 and node 1 of list2
+  // check for equivalence of these nodes
+  // if equal return node else get next of both nodes
+  // and repeat until there is no next
+}
+
+// ----------------------------------------
+// Given: Singly Linked List - Do Not Edit!
+// ----------------------------------------
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  addToTail(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+    }
+
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+}
+
+// --------------------------------------
+// Helper For Testing Only - Do Not Edit!
+// --------------------------------------
+var stringify = function(list) {
+  var result = [];
+  while(list !== null) {
+    result.push(list.value);
+    list = list.next;
+  }
+  return result.join("");
+}
+
+exports.Node = Node;
+exports.LinkedList = LinkedList;
+exports.linkedListIntersection = linkedListIntersection;
+exports.stringify = stringify;
